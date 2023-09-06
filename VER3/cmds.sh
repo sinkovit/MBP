4d7633811caefccfe46af4eccbafa9a3be2f6588
sinkovit <sinkovit@sdsc.edu>
2023-09-06 14:10:38 -0700

    Adds latest VER3 improvements

--- VER3/cmds.sh
+++ VER3/cmds.sh
@@ -1,21 +1,13 @@
-# This script does the follow processing steps the polynomial code
-# Gets rid of simple assignments of form "t[1] = a[2];" and "t[3] = x[4];"
-# Replaces usage of elements of t with corresponding elements of a and x 
+# This script moves all statements of form t[i] = a[j] * x[k] to top
+# of polynomial and sort order by the index of x then index of a. This
+# allows the elements of x to stay in register and be reused many
+# times. Then it renumbers the elements of a to remove the gaps in usage.
 
-# Convert [..] to <..> for clarity and save in temp1
-sed 's/\[/</'g body1.cpp | sed 's/\]/>/'g > temp1
-
-# Strip out lines that are simple substitutions (e.g., "t<1> = a<11>;") and save in temp2
-awk '$0 !~ /t<[0-9]*> = [ax]<[0-9]*>;/' temp1 > temp2
-
-# Generate sed commands to do substitutions (e.g., globally replace t<1> with a<11>, t<2> with a<204>, etc.)
-awk '/t<[0-9]*> = [ax]<[0-9]*>;/ {print "sed \047s/" $1 "/" $3 "/g\047 temp2 > temp3; mv temp3 temp2"}' temp1 | sed 's/>;/>/' > sedcmds
-
-# Execute sed commands
+sed 's/\[/< /'g body2.cpp | sed 's/\]/ >/'g > temp1 
+awk '/t< [0-9]* > = [ax]< [0-9]* > \* [ax]< [0-9]* >;$/ {print}' temp1 | sort -n -k10 -k6 > part1
+awk '$0 !~ /t< [0-9]* > = [ax]< [0-9]* > \* [ax]< [0-9]* >;$/ {print}' temp1 > part2
+cat part1 part2 > temp2
+awk 'BEGIN {RS=";\n"} {print "sed \047s/t< " $2 " >/z< " NR " >/g\047 temp2 > temp3; mv temp3 temp2"}' temp2 > sedcmds
 . sedcmds
-
-# Convert <..> back to [..]
-sed 's/</\[/'g temp2 | sed 's/>/\]/'g  > body2.cpp
-
-# Cleanup work
-rm -f temp1 temp2 sedcmds
+sed 's/< /\[/g; s/ >/\]/g; s/z/t/g' temp2 >  body3.cpp
+rm -f temp1 temp2 part1 part2 sedcmds
