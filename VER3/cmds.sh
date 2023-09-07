380fb285cfaa3a704371af2243ef3be85f6566e8
sinkovit <sinkovit@sdsc.edu>
2023-09-07 11:02:21 -0700

    Continued cleanup

--- VER3/cmds.sh
+++ VER3/cmds.sh
@@ -3,11 +3,15 @@
 # allows the elements of x to stay in register and be reused many
 # times. Then it renumbers the elements of a to remove the gaps in usage.
 
-sed 's/\[/< /'g body2.cpp | sed 's/\]/ >/'g > temp1 
+sed 's/\[/< /g; s/\]/ >/g' bodyf2.cpp > temp1 
 awk '/t< [0-9]* > = [ax]< [0-9]* > \* [ax]< [0-9]* >;$/ {print}' temp1 | sort -n -k10 -k6 > part1
 awk '$0 !~ /t< [0-9]* > = [ax]< [0-9]* > \* [ax]< [0-9]* >;$/ {print}' temp1 > part2
 cat part1 part2 > temp2
 awk 'BEGIN {RS=";\n"} {print "sed \047s/t< " $2 " >/z< " NR " >/g\047 temp2 > temp3; mv temp3 temp2"}' temp2 > sedcmds
 . sedcmds
-sed 's/< /\[/g; s/ >/\]/g; s/z/t/g' temp2 >  body3.cpp
+sed 's/< /\[/g; s/ >/\]/g; s/z/t/g' temp2 >  bodyf3.cpp
 rm -f temp1 temp2 part1 part2 sedcmds
+
+
+# Reconstruct poly-3b-v2x
+#cat top.cpp bodyf3.cpp mid.cpp bodyp3.cpp tail.cpp > poly-3b-v2x-3.cpp
