017e6d380a3bb9d1d6a0130404175539981a7af3
sinkovit <sinkovit@sdsc.edu>
2023-09-08 13:46:13 -0700

    More work

--- VER4/cmds.sh
+++ VER4/cmds.sh
@@ -11,14 +11,16 @@ sed 's/\[/</g; s/\]/>/g' bodyf3.cpp > temp2
 awk 'BEGIN {RS=";\n"; ORS=";\n"} {gsub(/\n/, "", $0); gsub(/\+\s*/, "+ ", $0); gsub(/\*\s*/, "* ", $0); gsub(/=\s*/, "= ", $0); print}' temp2 > x
 mv x temp2
 
+# Generate sed commands to do substitutions
+awk -f genseds.awk temp2 > sedcmds.sh
+awk '{print "grep \047/" $1 "/\047 sedcmds.sh"}' twos > filter-sedcmds.sh
+. filter-sedcmds.sh > filtered-sedcmds.sh
+
 # Strip out lines that assign values to elements used only once after being defined
 awk '{print "grep -v \047" $1 " =\047 temp2 > temp3; mv temp3 temp2"}' twos > strip.sh
 . strip.sh
 
-# Generate and execute sed commands to do substitutions
-awk -f genseds.awk temp2 > sedcmds.sh
-awk '{print "grep \047/" $1 "/\047 sedcmds.sh"}' twos > filter-sedcmds.sh
-. filter-sedcmds.sh > filtered-sedcmds.sh
+# Execute sed commands
 . filtered-sedcmds.sh
 
 # Renumbering and cleanup
