e9549dfb33f469dfb9aadb9c52744af1cbedf30b
sinkovit <sinkovit@sdsc.edu>
2023-09-08 17:50:37 -0700

    ver4 cleanups

--- VER4/cmds.sh
+++ VER4/cmds.sh
@@ -1,11 +1,11 @@
 # Remove semicolons and parentheses
-sed 's/;//g; s/(//g; s/)//g' bodyf3.cpp bodyp3.cpp > temp1
+sed 's/;//g; s/(//g; s/)//g' bodyp3.cpp > temp1
 
 # Find elements of array t that are only used once after being defined
 awk -f findtwos.awk temp1 > twos
 
 # Convert [] to <>
-sed 's/\[/</g; s/\]/>/g' bodyf3.cpp > temp2
+sed 's/\[/</g; s/\]/>/g' bodyp3.cpp > temp2
 
 # Convert multiline expressions to single line
 awk 'BEGIN {RS=";\n"; ORS=";\n"} {gsub(/\n/, "", $0); gsub(/\+\s*/, "+ ", $0); gsub(/\*\s*/, "* ", $0); gsub(/=\s*/, "= ", $0); print}' temp2 > x
@@ -23,21 +23,15 @@ awk '{print "grep -v \047" $1 " =\047 temp2 > temp3; mv temp3 temp2"}' twos > st
 # Execute sed commands
 . filtered-sedcmds.sh
 
-# Renumbering and cleanup of bodyf3
+# Renumbering and cleanup
 sed 's/</< /g; s/>/ >/g' temp2 > temp3
 awk 'BEGIN {RS=";\n"} {print "sed \047s/t< " $2 " >/z< " NR " >/g\047 temp3 > temp4; mv temp4 temp3"}' temp3 > sedcmds
 . sedcmds
-sed 's/< /\[/g; s/ >/\]/g; s/z/t/g' temp3 >  bodyf4.cpp
-
-# Now apply renumbering to bodyp3.cpp and do cleanup
-sed 's/\[/< /g; s/\]/ >/g' bodyp3.cpp > temp3
-. sedcmds
 sed 's/< /\[/g; s/ >/\]/g; s/z/t/g' temp3 >  bodyp4.cpp
-
 
 # Delete temp files
 #rm -f temp1 temp2 temp3 twos strip.sh sedcmds sedcmds.sh filter-sedcmds.sh filtered-sedcmds.sh
 
 # Reconstruct poly-3b-v2x
-cat top.cpp bodyf4.cpp mid.cpp bodyp4.cpp tail.cpp > poly-3b-v2x-4.cpp
+cat top.cpp bodyp4.cpp tail.cpp > poly-3b-v2x-4.cpp
 
